2bde44e00adbcdd4648d67cee0b4e56328d60abb
mafutka <mafutka@gmail.com>
2025-07-13 09:30:34 +0300

    types

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,6 +1,14 @@
 import styles from './Heading.module.css';
 
-export default function Heading({ title, top, bottom, error, info }) {
+export type HeadingProps = {
+  title: string;
+  top: string;
+  bottom: string;
+  error: string;
+  info: string;
+};
+
+export default function Heading({ title, top, bottom, error, info }: HeadingProps) {
   let className = styles.title;
 
   if (top) className += ` ${styles.top}`;
